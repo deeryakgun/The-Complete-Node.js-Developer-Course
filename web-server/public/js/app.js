3c716a21874b7b9a836cb470b7a975f30a187783
Derya Akgün <akgunderyaa@gmail.com>
2025-09-15 21:35:17 +0300

    web-server added

---
+++ web-server/public/js/app.js
@@ -0,0 +1,13 @@
+console.log('Client side javascript file is loaded.')
+
+fetch('http://localhost:3000/weather?address=istanbul').then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error)
+        }
+        else{
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
